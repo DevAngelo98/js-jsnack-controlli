ab6b23cbcdc3fe551e6347795106ddde81e5e51b
DevAngelo98 <ainglima07@gmail.com>
2019-10-27 16:04:00 +0100

    restyle js1

--- jsnack-2/assets/js/script.js
+++ jsnack-2/assets/js/script.js
@@ -1,10 +1,17 @@
 var primaParola = prompt("Inserisci la prima parola");
 var secondaParola = prompt("Inserisci la seconda parola");
 
-if (primaParola > secondaParola) {
-  document.getElementById("box").innerHTML = 'La parola più corta è: ' +  secondaParola + "<br>" + ' La parola più lunga è: ' +  primaParola;
-} else if(primaParola < secondaParola) {
-  document.getElementById("box").innerHTML = 'La parola più corta è: ' +  primaParola + "<br>" +' La parola più lunga è: ' +  secondaParola;
+var test = /^[a-z]+$/i;
+
+if(test.test(primaParola) && test.test(secondaParola)){
+  if (primaParola > secondaParola) {
+    document.getElementById("box").innerHTML = 'La parola più corta è: ' +  secondaParola + "<br>" + ' La parola più lunga è: ' +  primaParola;
+  } else if(primaParola < secondaParola) {
+    document.getElementById("box").innerHTML = 'La parola più corta è: ' +  primaParola + "<br>" +' La parola più lunga è: ' +  secondaParola;
+  } else {
+    document.getElementById("box").innerHTML = 'Hanno la stessa dimensione: ' + "<br>" +  "La prima parola inserita è: " + primaParola + "<br>" + "La seconda parola inserita è: " + secondaParola;
+  }
 } else {
-  document.getElementById("box").innerHTML = 'Hanno la stessa dimensione: ' + "<br>" +  "La prima parola inserita è: " + primaParola + "<br>" + "La seconda parola inserita è: " + secondaParola;
+  console.log("NIO");
 }
+
--- jsnack-4/assets/js/script.js
+++ jsnack-4/assets/js/script.js
@@ -1,15 +1,19 @@
 var nomi = ["Angelo", "Giuseppe", "Filippo", "Chiara"];
 var nomeInserito = prompt("Inserisci il tuo nome per sapere se sei stato invitato: ");
 var trovato = false;
+var test = /^[a-z]+$/i;
 
-for(var i=0; i<nomi.length && trovato==false; i++){
-  if(nomeInserito === nomi[i]){
-    document.getElementById("box").innerHTML = "Sei stato/a invitato/a";
-    trovato = true;
-  } else {
-    document.getElementById("box").innerHTML = "Mi dispiace non sei stato/a invitato/a";
+if (test.test(nomeInserito)) {
+  for(var i=0; i<nomi.length && trovato==false; i++){
+    if(nomeInserito === nomi[i]){
+      document.getElementById("box").innerHTML = "Sei stato/a invitato/a";
+      trovato = true;
+    } else {
+      document.getElementById("box").innerHTML = "Mi dispiace non sei stato/a invitato/a";
+    }
   }
 }
+
 
 // if(trovato == true){
 //   document.getElementById("box").innerHTML = "Sei stato/a invitato/a";
