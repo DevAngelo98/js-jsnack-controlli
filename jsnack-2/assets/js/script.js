784f1729e15c0e08ccc1c14792f3d607c2562579
DevAngelo98 <ainglima07@gmail.com>
2019-10-27 16:31:18 +0100

    restyle js2

--- jsnack-2/assets/js/script.js
+++ jsnack-2/assets/js/script.js
@@ -1,17 +1,26 @@
-var primaParola = prompt("Inserisci la prima parola");
-var secondaParola = prompt("Inserisci la seconda parola");
+
 
 var test = /^[a-z]+$/i;
 
-if(test.test(primaParola) && test.test(secondaParola)){
-  if (primaParola > secondaParola) {
-    document.getElementById("box").innerHTML = 'La parola più corta è: ' +  secondaParola + "<br>" + ' La parola più lunga è: ' +  primaParola;
-  } else if(primaParola < secondaParola) {
-    document.getElementById("box").innerHTML = 'La parola più corta è: ' +  primaParola + "<br>" +' La parola più lunga è: ' +  secondaParola;
+document.getElementById("check").onclick = function(){
+
+  var firstWord = document.getElementById("word1").value;
+  var secondWord = document.getElementById("word2").value;
+  var result = document.getElementById("result");
+
+  if(test.test(firstWord) && test.test(secondWord)){
+    if (firstWord.length > secondWord.length) {
+       result.innerHTML = 'The shortest word is: ' +  secondWord + "<br>" + ' The longest word is: ' +  firstWord;
+    } else if(firstWord.length < secondWord.length) {
+      result.innerHTML = 'The shortest word is: ' +  firstWord + "<br>" +' The longest word is: ' +  secondWord;
+    } else {
+      result.innerHTML = 'They have the same size: ' + "<br>" + "<br>" +  "The first word entered is: " + firstWord + "<br>" + "The second word entered is: " + secondWord;
+    }
   } else {
-    document.getElementById("box").innerHTML = 'Hanno la stessa dimensione: ' + "<br>" +  "La prima parola inserita è: " + primaParola + "<br>" + "La seconda parola inserita è: " + secondaParola;
-  }
-} else {
-  console.log("NIO");
+    result.innerHTML = "Incorrect values, try again!";
+    document.getElementById("word1").value = null;
+    document.getElementById("word2").value = null;
+  }  
 }
+
 
